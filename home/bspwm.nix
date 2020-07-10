46292d0154db3268c382aabb25433e0c433ee917
hss-mateus <hss-mateus@tuta.io>
2020-07-10 11:18:19 -0300

    Update bspwm monitor

--- home/bspwm.nix
+++ home/bspwm.nix
@@ -1,7 +1,7 @@
 {
   xsession.windowManager.bspwm = {
     enable = true;
-    monitors.VGA-0 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
+    monitors.HDMI-0 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
     startupPrograms = [
       "wmname LG3D"
       "xsetroot -cursor_name left_ptr"
