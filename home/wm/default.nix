d783caaa788791ec1498a30256238d6729cae170
hss-mateus <hss-mateus@pm.me>
2024-08-09 18:21:45 -0300

    Fix hyprlock not executing

--- home/wm/default.nix
+++ home/wm/default.nix
@@ -10,7 +10,17 @@
     enable = true;
 
     settings = {
-      listener.timeout = 300;
+      listener = [
+        {
+          timeout = 300;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 600;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
 
       general = {
         lock_cmd = "hyprlock";
