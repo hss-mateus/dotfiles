baa2ddc67fb47b9efff76ebb58b10d28099b93fe
hss-mateus <hss-mateus@pm.me>
2024-10-16 14:42:35 -0300

    Use desktop machine to run notebook builds

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,5 +1,12 @@
-{ inputs, ... }:
 {
+  inputs,
+  config,
+  lib,
+  ...
+}:
+{
+  options.ipv4Address = lib.mkOption { default = "192.168.0.151"; };
+
   imports = with inputs.nixos-hardware.nixosModules; [
     common-cpu-amd
     common-cpu-amd-pstate
@@ -7,5 +14,12 @@
     common-gpu-amd
     common-pc
     common-pc-ssd
+  ];
+
+  config.networking.interfaces.wlp3s0.ipv4.addresses = [
+    {
+      address = config.ipv4Address;
+      prefixLength = 24;
+    }
   ];
 }
--- hosts/notebook/default.nix
+++ hosts/notebook/default.nix
@@ -1,7 +1,30 @@
-{ inputs, ... }:
+{
+  inputs,
+  flake,
+  user,
+  ...
+}:
 {
   imports = [
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-e14-intel
     "${inputs.nixos-hardware}/common/gpu/intel/tiger-lake"
   ];
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      (
+        let
+          host = flake.outputs.nixosConfigurations.desktop.config;
+        in
+        {
+          hostName = host.ipv4Address;
+          sshUser = user;
+          system = host.nixpkgs.system;
+          maxJobs = 16;
+          supportedFeatures = host.nix.settings.system-features;
+        }
+      )
+    ];
+  };
 }
